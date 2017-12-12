5f936d1c2a95301cbe11ba25bc86bced4bda6be6
Martin <raichev93@gmail.com>
2017-12-12 20:16:17 +0200

    create relation

--- app/Http/Controllers/FindController.php
+++ app/Http/Controllers/FindController.php
@@ -11,7 +11,8 @@ class FindController extends Controller{
 
 	public function findFriends(){
 		$friends = User::where('country',1)->paginate(25);
-		
+		$friends1 = Friend::where('fr1','<>',1)->where('fr2', '<>', 1)->get();
+
 		return view('find', compact('friends'));
 	}
 
